cdf65cd85df8339560c0d76ccdd9baa22129d3cb
Robert Ayers <rsheldon3ayers@gmail.com>
2016-07-12 13:39:40 -0500

    rendered comments

--- client/components/Comments.js
+++ client/components/Comments.js
@@ -1,11 +1,29 @@
 import React from 'react'
-import Photo from './Photo'
+
+
 
 const Comments = React.createClass({
+    
+    renderComment(comment, i) {
+        return(
+        <div className='comment' key={i}>
+            <p>
+                <strong>{comment.user}</strong>
+                {comment.text}
+                <button className="remove-comment">&times;</button>
+            </p>
+        </div>
+        )
+},
     render(){
         return (
         <div className="comment">
-            I'm a comment'
+            {this.props.postComments.map(this.renderComment)}
+            <form ref="commentForm" className="comment-form">
+                <input type="text" ref="author" placeholder="author" />
+                <input type="text" ref="comment" placeholder="comment" />
+                <input type="submit" hidden />
+            </form>
         </div>
         )
     }
